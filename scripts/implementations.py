71824b6d5c32e6edac86db3a7ca45f86e38db6a4
Augustin Henry <augustin.henry@epfl.ch>
2021-10-07 16:28:04 +0200

    first function, a comparer

---
+++ scripts/implementations.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+
+###### Proposition Augustin ########
+
+def MSE(e):
+    return np.mean(e**2)/2
+
+
+def MAE(e):
+    return np.mean(np.abs(e))
+
+
+def compute_loss(y, tx, w, mse=True):
+    e = y - tx.dot(w)
+    if(mse):
+        return MSE(e)
+    else:
+        return MAE(e)
+
+
+def compute_gradient(y, tx, w):
+    e = y - tx.dot(w)
+    grad = -tx.T.dot(e)/len(e)
+    return grad, e
+
+
+def leastsquaresGD(y, tx, initial_w, max_iters, gamma):
+    w = initial_w
+    loss = 0
+    for n_iter in range(max_iters):
+
+        # calculate the gradient
+        grad, e = compute_gradient(y, tx, w)
+
+        # update the loss
+        loss = MSE(e)
+
+        # update w
+        w = w - gamma * grad
+
+    return loss, w
