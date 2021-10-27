cf3e02c30a1e396fae663efc5c484f1cd01f6b3f
JulietteChllt <63093857+JulietteChllt@users.noreply.github.com>
2021-10-27 18:14:38 +0200

    clean code and cross validation

--- scripts/implementations.py
+++ scripts/implementations.py
@@ -88,24 +88,6 @@ def ridge_regression(y, tx, lambda_):
     return w, compute_loss(y, tx, w)
 
 
-# # will most likely make your kernel die ...
-# def logistic_regression(y, tx, initial_w, max_iters, gamma):
-#     ws = [initial_w]
-#     losses = []
-#     w = initial_w
-#     for n_iter in range(max_iters):
-#         z = np.dot(tx, w)
-#         h = sigmoid(z)
-#         gd = np.dot(tx.T, (h - y))
-#         w -= gamma * gd
-#         loss = np.squeeze(-(np.dot(y.T, np.log(z)) +
-#                             np.dot((1 - y).T, np.log(1 - z))))
-#         ws.append(w)
-#         losses.append(loss)
-
-#     return w, loss
-
-
 # ---- logistic regression ----
 
 def sigmoid(z):
@@ -125,20 +107,89 @@ def calculate_gradient_sigmoid(y, tx, w):
     return tx.T.dot(pred - y)
 
 
+def logsig_log1pexp(t):
+    
+    (array1,array2,array3,array4) =(np.where(t<-33.3),np.where(((t<= -18) & (t>=-33.3))),np.where(((t<= 37) & (t>-18))),np.where(t >37))
+    tmp = np.ones(shape = t.shape)
+    tmp[array1]=t[array1]
+    tmp[array2]=t[array2]-np.exp(t[array2])
+    tmp[array3]=-np.log1p(np.exp(-t[array3]))
+    tmp[array4]=-np.exp(-t[array4])
+    return tmp
+
+
+def logsig(x):
+    out = np.zeros_like(x)
+    idx0 = x < -33
+    out[idx0] = x[idx0]
+    idx1 = (x >= -33) & (x < -18)
+    out[idx1] = x[idx1] - np.exp(x[idx1])
+    idx2 = (x >= -18) & (x < 37)
+    out[idx2] = -np.log1p(np.exp(-x[idx2]))
+    idx3 = x >= 37
+    out[idx3] = -np.exp(-x[idx3])
+    return out
+
+
+def calculate_loss(x, A, b):
+
+    z = np.dot(A, x)
+    b = np.asarray(b)
+    return np.mean((1 - b) * z - logsig(z))
+
+def expit_b(x, b):
+    idx = x < 0
+    out = np.zeros_like(x)
+    exp_x = np.exp(x[idx])
+    b_idx = b[idx]
+    out[idx] = ((1 - b_idx) * exp_x - b_idx) / (1 + exp_x)
+    exp_nx = np.exp(-x[~idx])
+    b_nidx = b[~idx]
+    out[~idx] = ((1 - b_nidx) - b_nidx * exp_nx) / (1 + exp_nx)
+    return out
+
+
+def calculate_gradient(x, A, b):
+
+    z = A.dot(x)
+    s = expit_b(z, b)
+    return A.T.dot(s) / A.shape[0] 
+
 def logistic_regression(y, tx, initial_w, max_iters, gamma):
+    # init parameters
+    threshold = 1e-8
+    losses = []
     w = initial_w
-    for iter in range(max_iters):
-        grad = calculate_gradient_sigmoid(y, tx, w)
-        w = w - grad * gamma
-    return w, calculate_loss_negative_likelihood(y, tx, w)
+
+    # start the logistic regression
+    for n_iter in range(max_iters):
+        loss = calculate_loss(w, tx, y)
+        gradient  = calculate_gradient(w, tx, y)
+        print(loss)
+        w= w - gamma*gradient
+
+        losses.append(loss)
+        if len(losses) > 1 and np.abs(losses[-1] - losses[-2]) < threshold:
+            print("I converged after this amount of iterations:",n_iter)
+            break
+            
+    return w.squeeze(),loss
 
 
 # ---- logistic regression regularized ----
 
 
-def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
+def reg_logistic_regression(y, tx, lambda_ ,initial_w, max_iters, gamma):    
+    threshold = 1e-8
+    losses = []
     w = initial_w
+  
     for iter in range(max_iters):
-        grad = calculate_gradient_sigmoid(y, tx, w) + 2 * lambda_ * w
-        w = w - gamma * grad
-    return w, calculate_loss_negative_likelihood(y, tx, w)
+        loss = calculate_loss(y, tx, w) + lambda_ * np.squeeze(w.T.dot(w))
+        gradient = calculate_gradient(y, tx, w) + 2 * lambda_ * w
+        w= w - gamma*gradient
+        losses.append(loss)
+        if len(losses) > 1 and np.abs(losses[-1] - losses[-2]) < threshold:
+            break
+            
+    return w.squeeze(),loss
